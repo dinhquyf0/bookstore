dec9ae70869d2d1dcd55d248a176ac9695cc877f
quy <dinhquyf8@gmail.com>
2016-06-21 13:23:33 +0700

    initize commit

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,35 @@
+class AuthorsController < ApplicationController
+  def new
+    @page_title = 'Add New Author'
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(author_params)
+    @author.save
+
+    redirect_to authors_path
+
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  private 
+    def author_params
+      params.require(:author).permit(:first_name, :last_name)
+    end
+
+end
